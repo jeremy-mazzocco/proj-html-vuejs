7780ad374fff52c4e99818f9f355bbbc0219a5a9
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-06-12 12:28:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from "./store";
+import PageHeader from "./components/PageHeader.vue";
+import PageMain from "./components/PageMain.vue";
+import PageFooter from "./components/PageFooter.vue";
+
+export default {
+    components: {
+        PageHeader,
+        PageMain,
+        PageFooter
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <PageHeader />
+    <PageMain />
+    <PageFooter />
+</template>
+
+<style></style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+    name: "PageHeader"
+}
+
+</script>
+
+<template>
+    <header>
+        <!-- header contact bar -->
+        <div class="container">
+            <div class="open-hours">
+                Open Hours: Mon-Sat - 9:00-18:00
+            </div>
+            <div class="contact">
+                <span class="phone-number">
+                    +1(305)1234-5678
+                </span>
+                <span class="email">
+                    hello@example.com
+                </span>
+                <span class="socials">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </span>
+            </div>
+        </div>
+        <!-- header jumbotron -->
+        <section>
+            <nav>
+                <div class="logo">
+                    <img src="../assets/svgs/svg-7.svg" alt="">
+                </div>
+                <div class="list">
+                    <ul>
+                        <li>home</li>
+                        <li>sevices</li>
+                        <li>about</li>
+                        <li>projects</li>
+                        <li>results</li>
+                    </ul>
+                    <button>
+                        get in touch
+                    </button>
+                </div>
+            </nav>
+
+        </section>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use './styles/partials/mixins.scss' as*;
+@use './styles/general.scss' as*;
+
+.container,
+nav,
+ul,
+.list {
+    @include my-dislay-flex;
+}
+
+.container {
+    height: 30px;
+
+}
+
+nav {
+    height: 50px;
+
+    .logo {
+        width: 140px;
+    }
+}
+
+section {
+    background-image: url(../assets/images/bg-parallax.png);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 500px;
+}
+</style>
